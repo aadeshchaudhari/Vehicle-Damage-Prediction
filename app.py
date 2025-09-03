dbf276ac44e35ccfc62823b159497986f5513abb
aadeshchaudhari <aadeshchaudhari1304@gmail.com>
2025-09-03 11:14:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,10 @@ st.title("Vehicle Damage Detection")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file:
-    # Create temporary file
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as tmp_file:
         tmp_file.write(uploaded_file.getbuffer())
         tmp_path = tmp_file.name
 
     st.image(uploaded_file, caption="Uploaded File", use_container_width=True)
-
-    # Make prediction
     prediction = predict(tmp_path)
     st.info(f"Predicted Class: **{prediction}**")
